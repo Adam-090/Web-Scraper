cf51d6183f245a3d059e249a286eba5868762e1d
Adam-090 <adam.hathiyari@gmail.com>
2024-03-03 17:08:03 -0600

    Figured out how to scrape the specific h2 tags and wrote a function to scrape h2 tags. H2 tags are important bc they are the titles of the articles.

--- # WebScraper.py
+++ # WebScraper.py
@@ -1,7 +1,17 @@
 # WebScraper
 from bs4 import BeautifulSoup
 import requests
-Url =  "https://www.newegg.com"
+baseUrl = "https://www.bbc.com"
+Url =  "https://www.bbc.com/news"
 webPage = requests.get(Url)
-print(webPage.text)
+#print(webPage.text)
 soup = BeautifulSoup(webPage.content,"html.parser")
+#print(soup.find('h2').text)
+def normalScrape():
+    h2tags=soup.find_all('h2')
+    count =0
+    for title in h2tags:
+        count +=1
+
+    print(count)
+normalScrape()
