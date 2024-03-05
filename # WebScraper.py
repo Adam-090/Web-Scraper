b17042f7d72597ae0a8d9858db73b66fbda36fca
Adam-090 <adam.hathiyari@gmail.com>
2024-03-04 23:45:50 -0600

    Finished up arguments for linux command line + Fine tuned function and what was being pulled

--- # WebScraper.py
+++ # WebScraper.py
@@ -1,17 +1,65 @@
 # WebScraper
 from bs4 import BeautifulSoup
 import requests
+import sys 
+mode=""
+type=""
+
+
+typearg = sys.argv[2:]
+
+for arg in typearg:
+    if arg == "-m":
+        mode = "normal"
+    elif arg == "-n":
+        type = "news"
+    elif arg == "-t":
+        type = "travel"
+    elif arg == "-b":
+        type = "business"
+    elif arg == "-c":
+        type = "culture"
+    elif arg == "-e":
+        type = "earth"
+    
+    
+        
+
+
+
+if type == "":
+    type = "news"
+if mode == "":
+    mode = "normal"
+
+
+
 baseUrl = "https://www.bbc.com"
-Url =  "https://www.bbc.com/news"
+Url =  ""
+
+if type == "news":
+    Url = baseUrl +"/news"
+elif type == "travel":
+    Url = baseUrl + "/travel"
+elif type == "business":
+    Url = baseUrl + "/business"
+elif type == "culture":
+    Url = baseUrl + "/culture"
+elif type == "earth":
+    Url = baseUrl + "/future-planet"
+
 webPage = requests.get(Url)
 #print(webPage.text)
 soup = BeautifulSoup(webPage.content,"html.parser")
 #print(soup.find('h2').text)
 def normalScrape():
     h2tags=soup.find_all('h2')
-    count =0
+  
     for title in h2tags:
-        count +=1
 
-    print(count)
+        print(title.text)
+
 normalScrape()
+    
+
+
